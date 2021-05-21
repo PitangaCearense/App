11dab7833d7bf58306bbfa281aadb1befd3f59fa
Joao Silva <josejoaosilva.n@gmail.com>
2021-05-21 11:52:02 -0300

    create project

---
+++ App/AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by José João Silva Nunes Alves on 21/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
